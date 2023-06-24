70313931dfed4436a7b8bf2606ceb04638432e66
Justin Stowe <justinstowe12@gmail.com>
2023-06-23 20:56:30 -0600

    filled in stats logic minus a database call.

--- app/components/modals/InformationModal.tsx
+++ app/components/modals/InformationModal.tsx
@@ -1,60 +1,84 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 
 export default function InformationModal() {
+  //useEffect to make call to database for "records"
+  //useState to save those values to be displayed
+  const [revenue, SetRevenue] = useState({
+    companyName: "null",
+    quarter: "null",
+    revenues: 0,
+  });
+  const [longestLifespan, setLongestLifespan] = useState({
+    companyName: "null",
+    days: 0,
+  });
+  const [cupsSold, setCupsSold] = useState(0);
+  const [bestMonth, setBestMonth] = useState({
+    companyName: "null",
+    quarter: 0,
+    earnings: 0,
+  });
+  const [bestDay, setBestDay] = useState({
+    companyName: "null",
+    cupsSold: 0,
+    earnings: 0,
+    temperature: "null",
+  });
   return (
     <div>
       <h2>Highest Revenue in a month:</h2>
       <ul>
         <li>
-          Company name: <span></span>
+          Company name: <span>{revenue.companyName}</span>
         </li>
         <li>
-          Quarter: <span></span>
+          Quarter: <span>{revenue.quarter}</span>
         </li>
         <li>
-          Revenues: <span></span>
+          Revenues: <span>{revenue.revenues}</span>
         </li>
       </ul>
       <h2>Overall: Longest Lifespan</h2>
       <ul>
         <li>
-          Company Name: <span></span>
+          Company Name: <span>{longestLifespan.companyName}</span>
         </li>
         <li>
-          <span></span>Days
+          <span>{longestLifespan.days} </span>Days
         </li>
       </ul>
       <h2>Total Cups Sold</h2>
       <ul>
         <li>
-          cups Sold:<span></span>
+          cups Sold:<span> {cupsSold}</span>
         </li>
       </ul>
       <h2>Highest Earnings in a month</h2>
       <ul>
         <li>
-          Company name: <span></span>
+          Company name: <span>{bestMonth.companyName}</span>
         </li>
         <li>
-          Quarter: <span></span>
+          Quarter: <span>{bestMonth.quarter}</span>
         </li>
         <li>
-          Earnings: <span></span>
+          Earnings: <span> {bestMonth.earnings}</span>
         </li>
       </ul>
       <h2>Overall: Best Day</h2>
       <ul>
         <li>
-          Company name: <span></span>
+          Company name: <span>{bestDay.companyName}</span>
         </li>
         <li>
-          cups sold: <span></span>
+          cups sold: <span>{bestDay.cupsSold}</span>
         </li>
         <li>
-          Earnings: <span></span>
+          Earnings: <span>{bestDay.earnings}</span>
         </li>
         <li>
-          Temperature: <span></span>
+          Temperature: <span>{bestDay.temperature}</span>
         </li>
       </ul>
     </div>
